96eb0a55761d83ed0923423e66eff6539ecc9d76
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 10:56:02 +0000

    Adding tarfile member sanitization to extractall()

--- geotorch/utility/_download_utils.py
+++ geotorch/utility/_download_utils.py
@@ -32,12 +32,50 @@ def _extract_archive(from_path, to_path) -> None:
 	if _is_tar(from_path):
 		print("Archive extraction started...")
 		with tarfile.open(from_path, 'r') as tar:
-			tar.extractall(path=to_path)
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, path=to_path)
 		print("Archive extraction finished")
 	elif _is_targz(from_path):
 		print("Archive extraction started...")
 		with tarfile.open(from_path, 'r:gz') as tar:
-			tar.extractall(path=to_path)
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, path=to_path)
 		print("Archive extraction finished")
 	elif _is_gzip(from_path):
 		print("Archive extraction started...")

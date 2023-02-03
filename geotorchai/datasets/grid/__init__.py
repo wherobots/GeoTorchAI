600868cfee98f7558dac26ee7466c790e52568ef
kanchanchy <kchowdh1@asu.edu>
2023-02-03 15:51:51 -0700

    precipitation data added

--- geotorchai/datasets/grid/__init__.py
+++ geotorchai/datasets/grid/__init__.py
@@ -2,6 +2,7 @@ from .nyc_bike_deepstn import BikeNYCDeepSTN
 from .nyc_bike_stdn import BikeNYCSTDN
 from .nyc_taxi_stdn import TaxiNYCSTDN
 from .taxi_bj_21 import TaxiBJ21
+from .total_precipitation import TotalPrecipitation
 from .processed import Processed
 
-__all__ = ["BikeNYCDeepSTN", "BikeNYCSTDN", "TaxiNYCSTDN", "TaxiBJ21", "Processed"]
+__all__ = ["BikeNYCDeepSTN", "BikeNYCSTDN", "TaxiNYCSTDN", "TaxiBJ21", "TotalPrecipitation", "Processed"]
--- geotorchai/utility/_download_utils.py
+++ geotorchai/utility/_download_utils.py
@@ -5,6 +5,7 @@ import tarfile
 import zipfile
 import torch
 from torch.utils.model_zoo import tqdm
+import cdsapi
 from .exceptions import FileDownloadException, ExtractArchiveException
 
 
@@ -14,7 +15,7 @@ def _download_remote_file(file_url: str, save_path: str) -> None:
 	full_save_path = os.path.join(save_path, file_name)
 
 	for _ in range(4):
-		with urllib.request.urlopen(urllib.request.Request(file_url, headers = {"Method": "HEAD", "User-Agent": "pytorch/geotorch"})) as response:
+		with urllib.request.urlopen(urllib.request.Request(file_url, headers = {"Method": "HEAD", "User-Agent": "pytorch/geotorchai"})) as response:
 			if response.url == file_url or response.url is None:
 				break
 			file_url = response.url
@@ -22,7 +23,7 @@ def _download_remote_file(file_url: str, save_path: str) -> None:
 		raise FileDownloadException("Unable to download the requested file")
 
 	print("File downloading started...")
-	with urllib.request.urlopen(urllib.request.Request(file_url, headers={"User-Agent": "pytorch/geotorch"})) as response:
+	with urllib.request.urlopen(urllib.request.Request(file_url, headers={"User-Agent": "pytorch/geotorchai"})) as response:
 		_save_chunk(iter(lambda: response.read(1024 * 32), b""), full_save_path, response.length)
 	print("File downloading finished")
 
@@ -79,6 +80,43 @@ def _is_gzip(filename):
 
 def _is_zip(filename):
 	return filename.endswith(".zip")
+
+
+def _download_single_cdsapi_file(save_path: str, variable, years, months, days, times, level_type, pressure_level, grid, product_type, format_name):
+	if level_type == 'pressure':
+		file_name = variable + '_' + pressure_level + '_' + year + '.nc'
+	else:
+		file_name = variable + '_' + years[0] + '.nc'
+
+	api = cdsapi.Client()
+
+	request_parameters = {
+	'product_type':   product_type,
+	'format':         format_name,
+	'variable':       variable,
+	'year':           years,
+	'month':          months,
+	'day':            days,
+	'time':           times,
+	}
+	if level_type == 'pressure':
+		request_parameters.update({'pressure_level': pressure_level})
+	if grid != None:
+		request_parameters.update({'grid': grid})
+
+	api.retrieve(
+		f'reanalysis-era5-{level_type}-levels',
+		request_parameters,
+		save_path + '/' + file_name
+		)
+
+
+def _download_cdsapi_files(save_path: str, variable, years, months, days, times, level_type, pressure_level = None, grid = None, product_type = 'reanalysis', format_name = 'netcdf'):
+	os.makedirs(save_path, exist_ok=True)
+
+	for year in years:
+		_download_single_cdsapi_file(save_path, variable, [year], months, days, times, level_type, pressure_level = pressure_level, grid = grid, product_type = product_type, format_name = format_name)
+
 
 
 

c299c92bdd4954705cd2da89cc10a5dd36b44325
kanchanchy <kchowdh1@asu.edu>
2023-02-03 17:07:39 -0700

    added temperature weather data

--- geotorchai/datasets/grid/__init__.py
+++ geotorchai/datasets/grid/__init__.py
@@ -3,6 +3,7 @@ from .nyc_bike_stdn import BikeNYCSTDN
 from .nyc_taxi_stdn import TaxiNYCSTDN
 from .taxi_bj_21 import TaxiBJ21
 from .total_precipitation import TotalPrecipitation
+from .temperature import Temperature
 from .processed import Processed
 
-__all__ = ["BikeNYCDeepSTN", "BikeNYCSTDN", "TaxiNYCSTDN", "TaxiBJ21", "TotalPrecipitation", "Processed"]
+__all__ = ["BikeNYCDeepSTN", "BikeNYCSTDN", "TaxiNYCSTDN", "TaxiBJ21", "TotalPrecipitation", "Temperature", "Processed"]

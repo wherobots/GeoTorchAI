454556447dcbf305f0d86d5aa9e0ae76d7a5d7ff
kanchanchy <kchowdh1@asu.edu>
2023-05-03 14:10:14 -0700

    Datasets tests updated

--- tests/deep-learning/test_grid_datasets.py
+++ tests/deep-learning/test_grid_datasets.py
@@ -4,14 +4,9 @@ from geotorchai.datasets.grid import BikeNYCDeepSTN, TaxiBJ21
 class TestGridDatasets:
 
 
-	def test_bike_nyc_deepstn_dataset_training_length(self):
+	def test_bike_nyc_deepstn_dataset_length(self):
 		data = BikeNYCDeepSTN(root = "data/partial_datasets/grid1")
-		assert len(data) == 646
-
-
-	def test_bike_nyc_deepstn_dataset_testing_length(self):
-		data = BikeNYCDeepSTN(root = "data/partial_datasets/grid1")
-		assert len(data) == 146
+		assert len(data) == 792
 
 
 	def test_bike_nyc_deepstn_dataset_closeness(self):
@@ -38,28 +33,23 @@ class TestGridDatasets:
 		assert sample[0] == 2 and sample[1] == 21 and sample[2] == 12
 
 
-	def test_taxi_bj21_dataset_training_length(self):
+	def test_taxi_bj21_dataset_length(self):
 		data = TaxiBJ21(root = "data/partial_datasets/grid2")
-		data.merge_closeness_period_trend(24, 1)
-		assert len(data) == 1257
+		data.set_sequential_representation(24, 1)
+		assert len(data) == 1399
 
-
-	def test_taxi_bj21_dataset_testing_length(self):
-		data = TaxiBJ21(root = "data/partial_datasets/grid2")
-		data.merge_closeness_period_trend(24, 1)
-		assert len(data) == 142
 
 
 	def test_taxi_bj21_dataset_history(self):
 		data = TaxiBJ21(root = "data/partial_datasets/grid2")
-		data.merge_closeness_period_trend(24, 1)
+		data.set_sequential_representation(24, 1)
 		sample = data[0]["x_data"].shape
 		assert sample[0] == 24 and sample[1] == 2 and sample[2] == 32 and sample[3] == 32
 
 
 	def test_taxi_bj21_dataset_predict(self):
 		data = TaxiBJ21(root = "data/partial_datasets/grid2")
-		data.merge_closeness_period_trend(24, 1)
+		data.set_sequential_representation(24, 1)
 		sample = data[0]["y_data"].shape
 		assert sample[0] == 1 and sample[1] == 2 and sample[2] == 32 and sample[3] == 32
 

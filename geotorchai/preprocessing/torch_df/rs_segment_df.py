59da632c9460aacd57b54987c5a4c60fd476dd97
Kanchan Chowdhury <43951087+kanchanchy@users.noreply.github.com>
2023-08-02 00:38:11 -0700

    Solved serialization problem

--- geotorchai/preprocessing/torch_df/rs_segment_df.py
+++ geotorchai/preprocessing/torch_df/rs_segment_df.py
@@ -10,25 +10,21 @@ from geotorchai.preprocessing.spark_registration import SparkRegistration
 
 class RasterSegmentationDf:
 
-    def __init__(self, df_raster, col_data, col_label, height, width, n_bands, is_label_masked=True, masking_threshold=255, transform: Optional[Callable] = None):
+    def __init__(self, df_raster, col_data, col_label, is_label_masked=True, masking_threshold=255):
         self.df_raster = df_raster
         self.col_data = col_data
         self.col_label = col_label
-        self.height = height
-        self.width = width
-        self.n_bands = n_bands
         self.is_label_masked = is_label_masked
         self.masking_threshold = masking_threshold
-        self.transform = transform
 
-
-    def __transform_row(self, batch_data):
-        transformers = [transforms.Lambda(lambda x: x.reshape((self.n_bands, self.height, self.width)))]
-        if self.transform != None:
-            transformers.extend([self.transform])
+    @classmethod
+    def __transform_row(cls, batch_data, n_bands, height, width, transform: Optional[Callable]):
+        transformers = [transforms.Lambda(lambda x: x.reshape((n_bands, height, width)))]
+        if transform != None:
+            transformers.extend([transform])
         trans = transforms.Compose(transformers)
 
-        transformers_label = [transforms.Lambda(lambda x: x.reshape((self.height, self.width)))]
+        transformers_label = [transforms.Lambda(lambda x: x.reshape((height, width)))]
         trans_label = transforms.Compose(transformers_label)
 
         batch_data['image_data'] = batch_data['image_data'].map(lambda x: trans(x))
@@ -53,8 +49,8 @@ class RasterSegmentationDf:
         return formatted_df
 
 
-    def get_transform_spec(self):
-        return TransformSpec(partial(self.__transform_row),
-                             edit_fields=[('image_data', np.float32, (self.n_bands, self.height, self.width), False),
-                                          ('label', np.int32, (self.height, self.width), False)],
+    def get_transform_spec(self, n_bands, height, width, transform: Optional[Callable] = None):
+        return TransformSpec(partial(RasterSegmentationDf.__transform_row, n_bands, height, width, transform),
+                             edit_fields=[('image_data', np.float32, (n_bands, height, width), False),
+                                          ('label', np.int32, (height, width), False)],
                              selected_fields=['image_data', 'label'])
